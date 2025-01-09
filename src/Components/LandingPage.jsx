5f483a7035385ac404d1c5f4156b0e92df776145
Suresh-48 <83566549+Suresh-48@users.noreply.github.com>
2025-01-09 09:15:36 +0530

    Landing Page UI implementation

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { Card, Container } from 'react-bootstrap'
+import curveImg from './Images/curveImg.png'
+import aviarlogo from './Images/aviarlogo.png'
+import studentsImg from './Images/studentsImg.png'
+import loginArrow from './Images/loginArrow.png'
+import backImg3 from './Images/backImg3.png'
+import laptopImg from './Images/laptopImg.png'
+import coding from './Images/coding.png'
+import Layer2 from './Images/Layer2.png'
+import './CSS/LandingPage.css'
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="landing-page-content-main">
+       <div className="Img-shape">
+        <div className='aviarlogo'>
+           <img src={aviarlogo} alt="" className='aviarlogoImg' />
+        </div>
+       </div>
   )
 }
-
 export default LandingPage
