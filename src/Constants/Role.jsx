c8ae591b589a6a46d211743c0f21828bf74e9717
Preetha1408 <preetha.t@aviartechservices.com>
2025-01-10 09:40:07 +0530

    CHECK1

---
+++ src/Constants/Role.jsx
@@ -0,0 +1,11 @@
+export const ROLE_TEACHER = "teacher";
+
+export const ROLES_STUDENT ="student";
+
+export const ROLES_ADMIN = "admin";
+
+export const TEACHER_ACCOUNT_TYPE = "individual";
+
+export const TEACHER_ACCOUNT_COUNTRY = "India";
+
+export const TEACHER_ACCOUNT_CURRENCY = "INR";
