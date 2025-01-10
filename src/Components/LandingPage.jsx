ead3de19ffdc98ffe07b3006c80428a40be5afe0
Preetha1408 <preetha.t@aviartechservices.com>
2025-01-10 09:42:48 +0530

    CHECK1

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,27 +1,4 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import { Card, Container } from 'react-bootstrap'
-import curveImg from './Images/curveImg.png'
-import aviarlogo from './Images/aviarlogo.png'
-import studentsImg from './Images/studentsImg.png'
-import loginArrow from './Images/loginArrow.png'
-import backImg3 from './Images/backImg3.png'
-import laptopImg from './Images/laptopImg.png'
-import coding from './Images/coding.png'
-import Layer2 from './Images/Layer2.png'
-import './CSS/LandingPage.css'
-const LandingPage = () => {
-  return (
-    <div className="landing-page-content-main">
-       <div className="Img-shape">
-        <div className='aviarlogo'>
-           <img src={aviarlogo} alt="" className='aviarlogoImg' />
-        </div>
-       </div>
-  )
-}
-export default LandingPage
-=======
+
 import react from "react";
 import { Card, Container } from "react-bootstrap";
 import curveImg from "./LandingPageImg/curveImg.png";
@@ -86,4 +63,4 @@ const LandingPage = () => {
   );
 };
 export default LandingPage;
->>>>>>> Stashed changes
+
