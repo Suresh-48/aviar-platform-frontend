dd26a7dd20fa64f46833cca97c55a8d2cfd73617
PrakashMuthuraman <jayasuriya.v@aviartechservices.com>
2025-01-24 19:36:07 +0530

    student-dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './Components/Login.jsx'
+import Studentdashboard from './Components/Dashboard/Studentdashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Studentdashboard />
   </StrictMode>,
 )
