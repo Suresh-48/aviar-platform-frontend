e00b8274853903a8e66e1bba66fa43fc28b66aa1
Preetha1408 <preetha.t@aviartechservices.com>
2025-02-20 13:05:48 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
@@ -9,25 +8,8 @@ import LandingPage from './Components/LandingPage';
 import Dashboard from './Components/Dashboard.jsx';
 import AdminLogin from './Components/AdminLogin.jsx';
 import Login from './Components/Login.jsx';
-=======
-import React from 'react'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import Login from './Components/Login'
-import Studentsignup from'./Components/studentsignup'
-import Teachersignup from'./Components/Teachersignup'
-import Studentsidebar from './Components/Core/Studentsidebar'
-import Updatestudentdetail from'./Components/Editstudentdetail/Updatestudentdetail'
-import UpcomingSchedule1 from './Components/Studentupcomingschedule/Upcomingschedule1'
-import AllCourseList from './Components/Courselist/AllCourseList'
-import PublicFooter from './Components/PublicLayout/PublicFooter'
-// import Admindashboard from'./Components/Dashboard/AdminDashboard'
-// import Dashboard from './components/Dashboard'
-// import LandingPage from './Components/LandingPage'
-// import Admin from './Components/Admin'
-// import Teacher from './Components/Teacher'
-// import Student from './Components/Student'
-// import StudentDashboard from './Components/Dashboard/Studentdashboard'
->>>>>>> feature/login-page-UI
+import PublicLayout from './Components/PublicLayout/PublicLayout.jsx'
+
 
 const App = () => {
   return (
@@ -42,39 +24,18 @@ const App = () => {
       />
       <Router>
         <Routes>
-<<<<<<< HEAD
+          <PublicLayout>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/login" element={<Login />} />
           {/* Add a default route or redirect if needed */}
           <Route path="/" element={<Navigate to="/aviar" />} />
+          </PublicLayout>
         </Routes>
       </Router>
     </div>
   );
 };
-=======
-          <Route path='/' element={<Login />} />
-          <Route path='Studentsignup' element={<Studentsignup />} />
-          <Route path='Teachersignup' element={<Teachersignup />} />
-          <Route path='Studentsidebar' element={<Studentsidebar />} />
-          <Route path='/Updatestudentdetail' element={<Updatestudentdetail />} />
-          <Route path='/Upcomingschedule1' element={<UpcomingSchedule1 />} />
-          <Route path='AllCourseList' element={<AllCourseList />} />
-          {/* <Route path='Admindashboard' element={<Admindashboard />} /> */}
-            {/* <Route path='/' element={<LandingPage />} />
-            <Route path='dashBoard' element={<Dashboard />} />
-            <Route path='admin' element={<Admin />} />
-            <Route path='teacher' element={<Teacher />} />
-            <Route path='student' element={<Student />} /> */}
-            
-        </Routes>
-        <PublicFooter />
-    </BrowserRouter>
-  
-  )
-}
->>>>>>> feature/login-page-UI
 
 export default App;
